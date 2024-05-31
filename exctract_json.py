c6297385bb11bcf8783ae58d38107b9f49b82112
ulibk <timsidorin@gmail.com>
2024-05-31 19:25:47 +1000

    v 3.0

--- exctract_json.py
+++ exctract_json.py
@@ -4,12 +4,21 @@ import difflib
 with open('extracted_data.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 titles = []
+description =[]
+
 
 for item in data:
     titles.append(item['title'])
+    description.append((item["description"]))
 
 def find_most_similar_title(input_title, titles):
 
     similarity_ratio = [difflib.SequenceMatcher(None, input_title, title).ratio() for title in titles]
     max_index = similarity_ratio.index(max(similarity_ratio))
-    return titles[max_index]
+    response = [titles[max_index],description[max_index]]
+    return response
+
+
+quest = "Я ошибся при отправке денег. Могу отменить платеж?"
+
+print(find_most_similar_title(quest,titles))
